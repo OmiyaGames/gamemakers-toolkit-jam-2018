c58dba8bc4da97faf83b984e6e39461f34b9433c
Taro Omiya <taro.omiya@omiyagames.com>
2018-09-02 17:50:31 -0400

    Adding a target to indicate which tree is about to be grappled.

--- Assets/Project/Scripts/GrappleHook.cs
+++ Assets/Project/Scripts/GrappleHook.cs
@@ -18,8 +18,11 @@ namespace Project
         TreeGenerator treeList;
         [SerializeField]
         float minDistanceFromTree = 15f;
+        [SerializeField]
+        GameObject targetIndicator;
 
         Rope[] allRopes;
+        readonly HashSet<ElementStatus> connectedTrees = new HashSet<ElementStatus>();
 
         public ElementStatus ClosestTree
         {
@@ -30,11 +33,14 @@ namespace Project
                 ElementStatus returnTree = null;
                 foreach (ElementStatus tree in treeList.AllElements)
                 {
-                    compareDistance = Vector3.Distance(transform.position, tree.transform.position);
-                    if(compareDistance < minDistance)
+                    if (connectedTrees.Contains(tree) == false)
                     {
-                        returnTree = tree;
-                        minDistance = compareDistance;
+                        compareDistance = Vector3.Distance(transform.position, tree.transform.position);
+                        if (compareDistance < minDistance)
+                        {
+                            returnTree = tree;
+                            minDistance = compareDistance;
+                        }
                     }
                 }
                 return returnTree;
@@ -46,7 +52,7 @@ namespace Project
         {
             allRopes = new Rope[numRopes];
             allRopes[0] = originalRope;
-            for(int i = 1; i < allRopes.Length; ++i)
+            for (int i = 1; i < allRopes.Length; ++i)
             {
                 GameObject clone = Instantiate(originalRope.gameObject, originalRope.transform.parent);
                 clone.transform.localPosition = Vector3.zero;
@@ -59,45 +65,57 @@ namespace Project
         void Update()
         {
             ElementStatus tree = ClosestTree;
-            if (tree != null)
+            if ((connectedTrees.Count > 0) && (CrossPlatformInputManager.GetButtonDown("Fire2") == true))
             {
-                // FIXME: add something to indicate this is the closest tree
-            }
-            if (CrossPlatformInputManager.GetButtonDown("Fire1") == true)
-            {
-                if (tree != null)
-                {
-                    foreach (Rope rope in allRopes)
-                    {
-                        if (rope.IsVisible == false)
-                        {
-                            rope.IsVisible = true;
-                            rope.Joint.connectedBody = tree.Body;
-                            break;
-                        }
-                    }
-                }
-            }
-            else if (CrossPlatformInputManager.GetButtonDown("Fire2") == true)
-            {
-                foreach(Rope rope in allRopes)
+                foreach (Rope rope in allRopes)
                 {
                     rope.IsVisible = false;
                 }
+                connectedTrees.Clear();
+            }
+            else if ((tree != null) && (connectedTrees.Count < allRopes.Length))
+            {
+                // Add something to indicate this is the closest tree
+                targetIndicator.SetActive(true);
+                targetIndicator.transform.position = tree.transform.position;
+                ConnectToTree(tree);
+            }
+            else
+            {
+                targetIndicator.SetActive(false);
             }
 
             // Update visuals
             int i = 0;
             float lerp;
+            Vector3 playerPos = handPosition.position, ropePos;
             foreach (Rope rope in allRopes)
             {
-                if(rope.IsVisible == true)
+                if (rope.IsVisible == true)
                 {
-                    for(; i < rope.Line.positionCount; ++i)
+                    ropePos = rope.Joint.connectedBody.position;
+                    for (i = 0; i < rope.Line.positionCount; ++i)
                     {
                         lerp = i;
                         lerp /= (rope.Line.positionCount - 1);
-                        rope.Line.SetPosition(i, Vector3.Lerp(handPosition.position, rope.Joint.connectedBody.position, lerp));
+                        rope.Line.SetPosition(i, Vector3.Lerp(playerPos, ropePos, lerp));
+                    }
+                }
+            }
+        }
+
+        private void ConnectToTree(ElementStatus tree)
+        {
+            if (CrossPlatformInputManager.GetButtonDown("Fire1") == true)
+            {
+                foreach (Rope rope in allRopes)
+                {
+                    if (rope.IsVisible == false)
+                    {
+                        rope.IsVisible = true;
+                        rope.Joint.connectedBody = tree.Body;
+                        connectedTrees.Add(tree);
+                        break;
                     }
                 }
             }
